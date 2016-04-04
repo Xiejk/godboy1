1d3ccca308b9ad8b0baa6e8221731f0e22c30851
godboy <xiejk@aliyun.com>
2016-04-04 18:05:41 +0800

    this is my first git file

---
+++ Traffic.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace GodBoy.CL.Order.Enum
+{
+    /// <summary>
+    /// 交通
+    /// </summary>
+    public enum Traffic:byte
+    {
+        [Description("火车")]
+        Train = 1,
+        [Description("大巴")]
+        Bus = 2,
+        [Description("飞机")]
+        Flight = 3,
+        [Description("轮船")]
+        Other = 4,
+        [Description("徒步")]
+        OnFoot = 5
+    }
+}
